5606464610bb9acd4d37b6788edd0df1b5764538
Marius Buleandra <mbuleandra@gmail.com>
2017-12-26 12:39:13 -0800

    add server

--- server.go
+++ server.go
@@ -1,7 +1,25 @@
 package main
 
 import "fmt"
+import "html"
+import "log"
+import "net/http"
+
+import "github.com/gorilla/mux"
+
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func ProductsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+    r := mux.NewRouter()
+    r.HandleFunc("/", HomeHandler)
+    r.HandleFunc("/products", ProductsHandler)
+
+    log.Fatal(http.ListenAndServe(":8080", r))
 }
